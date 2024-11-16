51fb3cafde6500e4e798766847f5fd6f85b142e0
ruhuma <141302425+ruhuma@users.noreply.github.com>
2024-11-16 06:11:20 +0000

    sec

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name)
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/classification', methods=['GET','POST'])
+def hey():
+    return render_template('classification.html')
+
+if __name__ == '__main__':
+    app.run()
+        
